18414afec6ebf0d5ea72ef3ad8944e5d0db9d84d
Asaf Ohayon <asaf@sysbind.co.il>
2023-07-27 13:39:39 +0300

    started postgres machine

--- install-postgres-db.sh
+++ install-postgres-db.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
 set -e
+
+set_hostname()
+{
+    hostnamectl set-hostname alloy
+    sed -i 's/bullseye/postgres/g' /etc/hosts
+}
+
+set_hostname
+
+apt update
+
+apt install -y wget lsb-release gnupg2
+
+
+# Create the file repository configuration:
+echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+
+# Import the repository signing key:
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+
+# Update the package lists:
+apt update
+
+# Install the latest version of PostgreSQL.
+# If you want a specific version, use 'postgresql-12' or similar instead of 'postgresql':
+apt -y install postgresql-14
