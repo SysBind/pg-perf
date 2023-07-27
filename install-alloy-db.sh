b8e2ada2be6f59259932b348252092bd916bdf39
Asaf Ohayon <asaf@sysbind.co.il>
2023-07-27 12:29:51 +0300

    install-alloy-db: organize

--- install-alloy-db.sh
+++ install-alloy-db.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
-apt update
-apt install ca-certificates curl gnupg lsb-release curl gpg
-mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
-apt update
-apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+set_hostname()
+{
+    hostnamectl set-hostname alloy
+    sed -i 's/bullseye/alloy/g' /etc/hosts
+}
+
+install_docker()
+{
+    apt update
+    apt install -y ca-certificates curl gnupg lsb-release curl gpg
+    mkdir -m 0755 -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
+    apt update
+    apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+}
+
+install_gsutil()
+{
+    curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-440.0.0-linux-x86_64.tar.gz
+    tar xvf google-cloud-cli-440.0.0-linux-x86_64.tar.gz
+    ./google-cloud-sdk/install.sh
+}
+
+set_hostname
+install_docker
+install_gsutil
